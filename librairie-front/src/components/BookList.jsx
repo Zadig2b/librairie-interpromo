5e23cc114527e92fa70612ace5959ec8a9a9ae08
romain <romain.dugeay@gmail.com>
2024-08-26 11:22:20 +0200

    refactorisation: le fetch est maintenant réalisé au niveau de page.js (page principale de l'app). cela évite une duplication du fetch lors de l'import du composant BookList réalisé 2 fois dans page.js On passe les livres et le type de liste via les props depuis la page d'accueil vers les composants BookList

--- librairie-front/src/components/BookList.jsx
+++ librairie-front/src/components/BookList.jsx
@@ -2,46 +2,14 @@
 import React, { useState, useEffect } from 'react';
 import BookTeaser from '../components/BookTeaser'; 
 
-export default function BookList(props) {
-    // Etat pour gérer les données du livre
-    const [books, setBooks] = useState([]);
-    // État pour gérer les états de chargement et d’erreur
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+export default function BookList({type, booksprops}) {
 
-    // Fetch books from the backend
-    useEffect(() => {
-        async function fetchBooks() {
-            try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/livres`);
-                // TODO: créer de nouvelles routes en utilisant /${props.type}
-                // on voudra alors récupérer les derniers livres ajoutés par le libraire si type=new
-                //ou les livres rangés par catégorie si type=categorie
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                
-                const data = await response.json();
-                setBooks(data);  // Update state with fetched data
-            } catch (error) {
-                setError(error.message);  // Update state with error message
-            } finally {
-                setLoading(false);  // Set loading to false once done
-            }
-        }
-        
-        fetchBooks();  // Call the fetch function
-    }, []);  // Empty dependency array means this runs once when the component mounts
-
-    // Render loading, error, or book list
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error}</p>;
 
     return (
         <div className="container">
-            <h1>Books List</h1>
+            <h1>{type}</h1>
             <ul className="list-unstyled">
-                {books.map(book => (
+                {booksprops?.map(book => (
                     <li key={book.id} className="mb-3">
                         <BookTeaser book={book} /> 
                     </li>
