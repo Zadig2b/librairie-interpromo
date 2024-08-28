4da5338a46356828dc625f3ca1a83ecd7ff66b46
herivola <rchristinella@gmail.com>
2024-08-28 11:59:45 +0200

    pagination

---
+++ librairie-front/src/components/Pagination.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+// Composant de pagination
+const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map(number => (
+          <li key={number} className="page-item">
+            <a
+              href="#!"
+              onClick={() => paginate(number)}
+              className="page-link"
+            >
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
