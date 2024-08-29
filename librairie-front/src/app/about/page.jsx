2d9176bf2786eb6b262c2362b83b38c2f02bc7b1
Artchibo7 <arthur.zulpukharov.simplon@gmail.com>
2024-08-29 14:24:23 +0200

    ajustement de section a porpos

--- librairie-front/src/app/about/page.jsx
+++ librairie-front/src/app/about/page.jsx
@@ -1,64 +1,91 @@
-"use client"
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 export default function About() {
-    return (
-        <div style={styles.container}>
-            <h1 style={styles.title}>À propos</h1>
-            <hr className="my-4" style={{border: '5px solid #BFE4FF' }} />
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>À propos</h1>
+      <hr className="my-4" style={{ border: "5px solid #BFE4FF" }} />
 
+      <p style={styles.paragraph}>
+        " La plume des Ecrins vous attend patiemment au bourg d'Oisans. A
+        proximité d'un cadre naturel époustouflant, venez faire le plein de
+        littérature avant de partir à l'aventure braver mère nature. Bande
+        dessinée, grands classique ou encore fantasy, notre collection
+        hétéroclite sera vous ravir ! " L’équipe de la librairie.
+      </p>
 
-            <p style={styles.paragraph}>
-                " La plume des Ecrins vous attend patiemment au bourg d'Oisans. A proximité d'un cadre naturel époustouflant, venez faire le plein de littérature avant de partir à l'aventure braver mère nature. Bande dessinée, grands classique ou encore fantasy, notre collection hétéroclite sera vous ravir ! " L’équipe de la librairie.
-            </p>
-
-            <div style={styles.imageContainer}>
-                <Image
-                    src="/Bibliothèque-INHA-Eklektike.jpg" // Assurez-vous que l'image est placée dans le dossier public
-                    alt="Librairie"
-                    width={800}
-                    height={450}
-                    style={styles.image}
-                />
-            </div>
-
-            <div style={styles.contact}>
-                <h2>Contactez-nous</h2>
-                <p><strong>Adresse :</strong> 25 rue Pierre Kropotkine
-                38 000 Grenoble, France</p>
-                <p><strong>Téléphone :</strong> +33 1 23 45 67 89</p>
-            </div>
+      <div style={styles.imageContainer}>
+        <Image
+          src="/Bibliothèque-INHA-Eklektike.jpg" // Assurez-vous que l'image est placée dans le dossier public
+          alt="Librairie"
+          width={800}
+          height={450}
+          style={styles.image}
+        />
+      </div>
+      <div
+        className="column d-flex justify-content-between align-items-center"
+      >
+        <div>
+          <p className="text-start">
+            <Image
+              src="/asset/Icones/compte/accueil.png"
+              alt=""
+              width={20}
+              height={20}
+            />{" "}
+            25 rue Pierre Kropotkine <br />38 000 Grenoble, France
+          </p>
+          <p>
+            <Image
+              src="/asset/Icones/compte/email.png"
+              alt=""
+              width={20}
+              height={20}
+            />
+            JohannKepler@gmail.com
+          </p>
         </div>
-    );
+        <div style={{ flex: 2 }}>
+          <p className="text-centre">
+            Vous pouvez contacter le siège social de <br />notre librairie à Grenoble
+            pour tout <br />renseignement supplémentaire <br />concernant la donation
+            d’ouvrage.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 // Styles inline pour la page
 const styles = {
-    container: {
-        padding: '20px',
-        maxWidth: '800px',
-        margin: '0 auto',
-        textAlign: 'center',
-    },
-    title: {
-        fontSize: '3rem',
-        marginBottom: '20px',
-    },
-    paragraph: {
-        fontSize: '1.2rem',
-        lineHeight: '1.6',
-        marginBottom: '40px',
-    },
-    imageContainer: {
-        marginBottom: '40px',
-    },
-    contact: {
-        fontSize: '1rem',
-        lineHeight: '1.5',
-    },
-    image: {
-        borderRadius: '10px',
-    },
+  container: {
+    padding: "20px",
+    maxWidth: "800px",
+    margin: "0 auto",
+    textAlign: "center",
+  },
+  title: {
+    fontSize: "3rem",
+    marginBottom: "20px",
+  },
+  paragraph: {
+    fontSize: "1.2rem",
+    lineHeight: "1.6",
+    marginBottom: "40px",
+  },
+  imageContainer: {
+    marginBottom: "40px",
+  },
+  contact: {
+    fontSize: "1rem",
+    lineHeight: "1.5",
+  },
+  image: {
+    borderRadius: "10px",
+  },
 };
