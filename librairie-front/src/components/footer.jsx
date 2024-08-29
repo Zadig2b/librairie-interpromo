40f4631059e126009feb3c65d75a9af350f21f88
Artchibo7 <arthur.zulpukharov.simplon@gmail.com>
2024-08-29 10:10:19 +0200

    ajustement footer

--- librairie-front/src/components/footer.jsx
+++ librairie-front/src/components/footer.jsx
@@ -8,14 +8,14 @@ export default function Footer() {
     <footer className=" text-white py-4" style={{ backgroundColor: "#A79A9C" }}>
       <div className="container">
         <div className="row">
-          <div className="col-md-4 text-center text-md-left">
+          <div className="col-md-4 text-center mt-1">
             <img
               src="../../asset/logo/blanc/logo-transparent-png.png"
               alt="Logo de la librairie"
               className="footer-logo"
-              style={{ width: "200px", marginRight: "70px", marginTop: "1px" }}
+              style={{ width: "250px"}}
             />
-            <div className="d-flex justify-content-center justify-content-md-start ">
+            <div className="d-flex justify-content-md-center ">
               <Link
                 href="https://twitter.com"
                 target="_blank"
@@ -24,8 +24,8 @@ export default function Footer() {
                 <Image
                   src="/asset/Icones/réseaux/x-twitter-brands-solid.svg"
                   alt="Logo"
-                  width={40}
-                  height={40}
+                  width={30}
+                  height={30}
                   className="me-2"
                   style={{ marginTop: "5px", color: "pink" }}
                 />
@@ -38,8 +38,8 @@ export default function Footer() {
                 <Image
                   src="/asset/Icones/réseaux/instagram-brands-solid.svg"
                   alt="Logo"
-                  width={40}
-                  height={40}
+                  width={30}
+                  height={30}
                   className="me-2"
                   style={{ marginTop: "5px", color: "pink" }}
                 />
@@ -52,68 +52,54 @@ export default function Footer() {
                 <Image
                   src="/asset/Icones/réseaux/facebook-brands-solid.svg"
                   alt="Logo"
-                  width={40}
-                  height={40}
+                  width={30}
+                  height={30}
                   className="me-2"
                 />
               </Link>
             </div>
           </div>
-          <div className="col-md-4 text-center text-md-right">
+          <div className="col-md-4 text-center mt-3 text-md-right">
             <h6>Contact</h6>
-            {/* <p>
-              Email: <a href="mailto:info@example.com" className="text-white">info@example.com</a>
-            </p> */}
-
             <p>
               <a
                 href="https://www.google.fr/maps/place/44+Rue+du+G%C3%A9n%C3%A9ral+de+Gaulle,+38520+Le+Bourg-d'Oisans/@45.0546229,6.0318106,17z/data=!4m6!3m5!1s0x478a6c1cef23ade9:0x7260d683bb170543!8m2!3d45.0546229!4d6.0318106!16s%2Fg%2F11c1z54pdn?entry=ttu&g_ep=EgoyMDI0MDgyMy4wIKXMDSoASAFQAw%3D%3D"
                 className="text-white"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+                style={{ textDecoration: "none" }}
+                target="_blank">
                 44 Rue du Général de Gaulle,
                 <br />
                 38520 Le Bourg-d'Oisans
-              </a>
-            </p>
-            <p>
-              <a href="tel:+1234567890" className="text-white">
+                <br />
                 04 76 80 08 47
               </a>
             </p>
           </div>
-          <div className="col-md-4 text-center">
-            {/* <h5>Liens rapides</h5> */}
+          <div className="col-md-4 text-center mt-4">
             <ul className="list-unstyled">
               <li>
-                <a href="/" className="text-white">
+                <a href="/" className="text-white" style={{ textDecoration: "none" }}>
                   Accueil
                 </a>
               </li>
               <li>
-                <a href="/books" className="text-white">
+                <a href="/books" className="text-white" style={{ textDecoration: "none" }}>
                   Nos Livres
                 </a>
               </li>
               <li>
-                <a href="/about" className="text-white">
+                <a href="/about" className="text-white" style={{ textDecoration: "none" }}>
                   {" "}
                   À Propos
                 </a>
               </li>
               <li>
-                <a href="/contact" className="text-white">
+                <a href="/contact" className="text-white" style={{ textDecoration: "none" }}>
                   Contact
                 </a>
               </li>
             </ul>
           </div>
-        </div>
-        <div className="row">
-          {/* <div className="col text-center mt-4">
-            <p>&copy; {new Date().getFullYear()} Ma librairie. Tous droits réservés.</p>
-          </div> */}
         </div>
       </div>
     </footer>
